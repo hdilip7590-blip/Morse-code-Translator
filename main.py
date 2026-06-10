f45e3702aa158c79474f0faf81fdf00cbf53ed67
hdilip7590-blip <hdilip7590@gmail.com>
2026-06-10 16:26:28 +0800

    Input Validation added

--- main.py
+++ main.py
@@ -10,32 +10,41 @@ MORSE_CODE_DICT = {
     '5': '.....', '6': '-....', '7': '--...', '8': '---..',
     '9': '----.', '0': '-----'
 }
-code_or_decode = int(input("Option 1: Code to Morse\nOption 2: Decode to text\nSelect your option: "))
-if code_or_decode == 1:
-    to_code = input("What word would you like to code: \n")
-    cleaned_code = ' '.join(to_code.split())
-    new = []
+while True:
+    try:
+        code_or_decode = int(input("Option 1: Code to Morse\nOption 2: Decode to text\nSelect your option: "))
+    except ValueError:
+        print("Enter Valid Interger between 1 and 2\n\n")
+    else:
+        if code_or_decode == 1:
+            to_code = input("\nWhat word would you like to code: \n")
+            cleaned_code = ' '.join(to_code.split())
+            new = []
 
-    for char in cleaned_code.upper():
-        if char in MORSE_CODE_DICT:
-            new.append(MORSE_CODE_DICT[char])
+            for char in cleaned_code.upper():
+                if char in MORSE_CODE_DICT:
+                    new.append(MORSE_CODE_DICT[char])
+                else:
+                    if char == " ":
+                        new.append("/")
+
+            print(' '.join(new))
+            break
+
+        elif code_or_decode == 2:
+            to_decode = input("\nWhat word would you like to decode: \n")
+            cleaned_code = to_decode.split()
+            new = []
+
+            for morse in cleaned_code:
+                if morse != "/":
+                    for char, value in MORSE_CODE_DICT.items():
+                        if morse==value:
+                            new.append(char)
+                else:
+                    new.append("/")
+
+            print(''.join(new).replace('/',' '))
+            break
         else:
-            if char == " ":
-             new.append("/")
-
-    print(' '.join(new))
-
-elif code_or_decode == 2:
-    to_decode = input("What word would you like to decode: \n")
-    cleaned_code = to_decode.split()
-    new = []
-
-    for morse in cleaned_code:
-        if morse != "/":
-            for char, value in MORSE_CODE_DICT.items():
-                if morse==value:
-                    new.append(char)
-        else:
-            new.append("/")
-
-    print(''.join(new).replace('/',' '))
+            print("Choose only between 1 and 2\n\n")
